ccf64a77f70edf8e5a4c499705820cbee8756c8b
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-06-09 22:55:46 +0900

    Update 1.5_numpy.py

--- 1.5-numpy/1.5_numpy.py
+++ 1.5-numpy/1.5_numpy.py
@@ -34,24 +34,38 @@ import numpy as np
 # a = np.uint8([1,2,3,4])
 
 # 크기와 초기 값으로 numpy array 생성
-a = np.empty([2,3]) # 쓰레기값으로 배열 생성 // 2 rows, 3 cols 2열 3행
-print(a)
-print(a.dtype) # float64
+# a = np.empty([2,3]) # 쓰레기값으로 배열 생성 // 2 rows, 3 cols 2열 3행
+# print(a)
+# print(a.dtype) # float64
 
-a.fill(255)
-print(a)
+# a.fill(255)
+# print(a)
 
-b = np.zeros([2,3])
+# b = np.zeros([2,3])
+# print(b)
+# print(b.dtype)
+
+# c = np.zeros([2,3], dtype=np.int8)
+# print(c)
+# print(c.dtype)
+
+
+# d = np.ones((2,3))
+# print(d)
+
+# e = np.full((2,2,2,1,1), 255) # 3차원 배열 (row, 1개 row기준 dimension, column)
+# print(e)
+
+# 기존 image를 가용하여 같은 shape을 갖는 np array 만들기
+img = cv2.imread("/Users/kis/Documents/GitHub/Computer-vision-study/1.5-numpy/white.png")
+print(img.shape)
+
+a = np.empty_like(img)
+b = np.zeros_like(img)
+c = np.ones_like(img)
+d = np.full_like(img, 220)
+
+print(a)
 print(b)
-print(b.dtype)
-
-c = np.zeros([2,3], dtype=np.int8)
 print(c)
-print(c.dtype)
-
-
-d = np.ones((2,3))
 print(d)
-
-e = np.full((2,2,2,1,1), 255) # 3차원 배열 (row, 1개 row기준 dimension, column)
-print(e)
